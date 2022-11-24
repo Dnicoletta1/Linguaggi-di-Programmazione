0413b69256873f8f9fdcf194fd30c9b9274ca413
Davide Nicoletta <d.nicoletta1@campus.unimib.it>
2022-11-24 12:33:27 +0100

    Aggiunta is_a_number/1 con cut

--- JSON.pl
+++ JSON.pl
@@ -1,14 +1,15 @@
 %%%% -*- Mode: Prolog -*-
 
 is_a_number([X]) :-
-    is_digit(X).
+    is_digit(X),!.
 
 is_a_number([X | Xs]) :-
     is_digit(X),
     is_a_number(Xs).
 
-is_a_mainus(X) :-
-	X = '-'.
+is_a_number(['-'| Xs]) :-
+	is_a_number(Xs).
+
     
     
 
