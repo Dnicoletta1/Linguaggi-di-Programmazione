a751c571d41d6b74fe69963cb3ca8e9546612705
Davide Nicoletta <d.nicoletta1@campus.unimib.it>
2022-11-24 14:09:25 +0100

    Aggiunta delle funzioni is_a_number/1  is_a_string/1  to_list/2

--- JSON.pl
+++ JSON.pl
@@ -1,5 +1,15 @@
 %%%% -*- Mode: Prolog -*-
 
+%%json/1
+%%start operation
+json(X) :-
+    string_to_list_of_characters(X, Y),
+    is_a_string(Y).
+
+
+%% is_a_number/1
+%% check that it is an positive, negative or decimal number
+
 is_a_number([X]) :-
     is_digit(X),!.
 
@@ -8,9 +18,32 @@ is_a_number([X | Xs]) :-
     is_a_number(Xs).
 
 is_a_number(['-'| Xs]) :-
-	is_a_number(Xs).
+    is_a_number(Xs).
 
-    
-    
+is_a_number(['.' | Xs]) :-
+    is_a_number(Xs).
+
+%% is_a_string/1
+%%check that it is an string
+
+is_a_string([X]) :-
+    atom_length(X,1),
+    char_type(X, lower),!.
+
+is_a_string([X | Xs]) :-
+    is_a_string([X]),
+    is_a_string(Xs).
+
+
+%%to_list/2
+%%conver string to list
+
+to_list(String, Characters) :-
+name(String, Xs),
+maplist( number_to_character,
+Xs, Characters ).
+
+number_to_character(Number, Character) :-
+name(Character, [Number]).
 
 %%%% end of file -- JSON.pl
